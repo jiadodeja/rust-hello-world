4c83b0a8195f71752b25f31d863bb238aeee17ac
Jia Dodeja <jiadodeja@gmail.com>
2025-09-09 18:49:11 -0400

    my hello world program in rust

---
+++ hello-world.rs
@@ -0,0 +1,8 @@
+//Jia Dodeja 
+//9/9/2025
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print "Hello World!" to the console.
+    println!("Hello World!");
+}   
